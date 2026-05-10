1d8b7090f9d55f440ae0324afdc202733af57128
Soroush Beigi <sbeygi712@gmail.com>
2026-05-10 18:56:42 +0330

    feat(domain): mapped business requirement to entities

--- entity/game.go
+++ entity/game.go
@@ -1,8 +1,24 @@
 package entity
 
 type Game struct {
-	ID        uint
-	Category  string
-	Questions []Question
-	Players   []User
+	ID          uint
+	Category    string
+	QuestionIDs []uint
+	PlayerIDs   []Player
+	WinnerID    uint
+}
+
+type Player struct {
+	ID      uint
+	UserID  uint
+	GameID  uint
+	Score   uint
+	Answers []PlayerAnswer
+}
+
+type PlayerAnswer struct {
+	ID         uint
+	PlayerID   uint
+	QuestionID uint
+	Choice     PossibleAnswerChoice
 }
--- entity/question.go
+++ entity/question.go
@@ -3,8 +3,49 @@ package entity
 type Question struct {
 	ID              uint
 	Question        string
-	PossibleAnswers []string
-	CorrectAnswer   []string
-	Difficulty      string
-	Category        string
+	PossibleAnswers []PossibleAnswer
+	CorrectAnswerID uint
+	Difficulty      QuestionDifficulty
+	CategoryID      uint
+}
+
+type PossibleAnswer struct {
+	ID      uint
+	Content string
+	Choice  PossibleAnswerChoice
+}
+
+type PossibleAnswerChoice uint8
+
+const (
+	PossibleAnswerA PossibleAnswerChoice = iota + 1
+	PossibleAnswerB
+	PossibleAnswerC
+	PossibleAnswerD
+)
+
+func (pac PossibleAnswerChoice) IsValid() bool {
+	if pac >= PossibleAnswerA && pac <= PossibleAnswerD {
+
+		return true
+	}
+
+	return false
+}
+
+type QuestionDifficulty uint8
+
+const (
+	QuestionDifficultyEasy QuestionDifficulty = iota + 1
+	QuestionDifficultyMedium
+	QuestionDifficultyHard
+)
+
+func (qd QuestionDifficulty) IsValid() bool {
+	if qd >= QuestionDifficultyEasy && qd <= QuestionDifficultyHard {
+
+		return true
+	}
+
+	return false
 }
