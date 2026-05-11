7b21fe4b03559adced667d6f372faaf73f2922cd
Soroush Beigi <sbeygi712@gmail.com>
2026-05-11 16:29:48 +0330

    feat(db): added mysql repo, docker compose setup, and mysql users table

---
+++ repository/mysql/user.go
@@ -0,0 +1,11 @@
+package mysql
+
+import "github.com/SoroushBeigi/knowledge-game/entity"
+
+func (db DB) IsPhoneNumberUnique(pn string) (bool, error) {
+
+}
+
+func (db DB) Register(u entity.User) (entity.User, error) {
+
+}
