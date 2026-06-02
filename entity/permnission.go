a38f10375402867ba94e0769e06ff5d940f1db94
Soroush Beigi <sbeygi712@gmail.com>
2026-06-02 16:04:47 +0330

    feat(auth): added mysql migrations to add admin permissions automatically

--- entity/permnission.go
+++ entity/permnission.go
@@ -6,7 +6,6 @@ type Permission struct {
 }
 
 // role-action
-// Must be inserted into database manually or written in migrations
 const (
 	UserListPermission   = "user-list"
 	UserDeletePermission = "user-delete"
